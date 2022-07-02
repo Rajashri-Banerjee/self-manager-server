b63e754b44dca2634e5045a6368e1d3db65cadb6
rootrsk <ravishankar7050@gmail.com>
2022-07-02 23:35:06 +0530

    setting home message

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express()
 dotenv.config()
 
 const port = process.env.PORT || 3001
-
+app.get('/',(req,res)=>{
+    res.json({
+        message:'Welecome to self-manager rest api'
+    })
+})
 app.listen(port,()=>{
     console.log("Server started at port : " +port)
 })
